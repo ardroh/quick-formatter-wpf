2aee1b350a1c0a807484794eafb194e0704ae3e6
Arkadiusz Drohomirecki <arkadiusz.drohomirecki@teleste.com>
2020-07-07 23:17:22 +0200

    Added status bar representing the number of Input/Output lines.

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Input;
 using quick_formatter_wpf.Commands;
 
@@ -36,6 +38,9 @@ namespace quick_formatter_wpf
 
         public string SelectedFormatter { get; set; }
 
+        private string _statusBarText;
+        public string StatusBarText { get => _statusBarText; set { _statusBarText = value; OnPropertyChanged(); } }
+
         public MainWindowViewModel()
         {
             AvailableFormatters = new List<string>()
@@ -51,7 +56,37 @@ namespace quick_formatter_wpf
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
+            if (name == "OutputText" || name == "InputText")
+            {
+                UpdateStatusBarText();
+            }
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void UpdateStatusBarText()
+        {
+            var statusBarTextBuilder = new StringBuilder();
+            if (InputText != null)
+            {
+                statusBarTextBuilder.Append($"Input lines: {CountLines(InputText)}");
+            }
+
+            if (InputText != null && OutputText != null)
+            {
+                statusBarTextBuilder.Append(" | ");
+            }
+
+            if (OutputText != null)
+            {
+                statusBarTextBuilder.Append($"Output lines: {CountLines(OutputText)}");
+            }
+
+            StatusBarText = statusBarTextBuilder.ToString();
+        }
+
+        private int CountLines(string text)
+        {
+            return Regex.Matches(text, "\n|\n\r").Count;
         }
     }
 }
